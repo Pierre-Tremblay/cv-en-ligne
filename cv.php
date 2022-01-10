d196d984def7d6df42b9212173d5616feda0abb5
Pierre Tremblay <pierre@studioplune.fr>
2022-01-10 01:15:04 +0100

    Ajout CV

---
+++ cv.php
@@ -0,0 +1,206 @@
+<head>
+    <title>CV de Pierre TREMBLAY</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="cv.css">
+</head>
+
+<body>
+    <header>
+        <h1>CV de Pierre TREMBLAY</h1>
+    </header>
+
+    <section>
+        <div class="photo">
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQE1oMnvradKhQ/profile-displayphoto-shrink_800_800/0/1594035804125?e=1646870400&v=beta&t=DGX-ihjXKMKN8FZUd2ZqKmBWpjv43oYp7QtCaDH0vTE" alt="Ma photo de profil">
+        </div>
+        <div class="prez">
+            <h2>Qui suis-je ?</h2>
+            <p>L’attrait pour le domaine du numérique a toujours été présent
+                durant mes études supérieures, c’est ainsi que je fais le choix
+                de me réorienter dans le domaine du numérique.
+                J’ai eu l’opportunité d’intégré l’école Arinfo et l’entreprise STUDIO PLUNE. Par le biais de ces deux entités, j’ai pu faire le lien entre théorie et pratique, en mettant en application les différents éléments abordés en cours. De plus mon projet de fin d’année utilise le framework Symfony.</p>
+            <a href="https://www.canva.com/design/DAEsEbunN8Y/P8s_MT7p8jyThpH3LHGnjQ/view?utm_content=DAEsEbunN8Y&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" download>Télécharger mon CV</a>
+        </div>
+        <div class="contact">
+            <h2>Informations de contact</h2>
+            <div class="contact-flex">
+                <p>Nom : </p>
+                <p>TREMBLAY Pierre</p>
+            </div>
+            <div class="contact-flex">
+                <p>Adresse : </p>
+                <p>2 avenue Jean Baptiste Perrin - 44300 NANTES</p>
+            </div>
+            <div class="contact-flex">
+                <p>Téléphone :</p>
+                <p>06 87 72 88 94</p>
+            </div>
+            <div class="contact-flex">
+                <p>Mail : </p>
+                <p><a href="mailto:pierre.tremblay.1995@gmail.com">pierre.tremblay.1995@gmail.com</a></p>
+            </div>
+            <div class="contact-flex">
+                <p>Permis :</p>
+                <p>B</p>
+            </div>
+            <div class="social">
+                <a href="linkedin.com/in/pierre-tremblay"><img src="https://www.pierre-giraud.com/wp-content/uploads/2019/07/lk.png" alt="Logo Lk"></a>
+                <a href="github.com/Pierre-Tremblay"><img src="https://www.egi.eu/wp-content/uploads/2021/03/github1600.png" alt="Logo Yt"></a>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <h2>Expériences professionnelles</h2>
+        <div class="exp">
+            <div class="exp-logo">
+                <a href="https://www.studioplune.fr/"><img src="https://www.studioplune.fr/wp-content/themes/plune/assets/img/logo.png" alt="Logo de Pierre Giraud"></a>
+            </div>
+            <div class="exp-info">
+                <h3>Développeur web</h3>
+                <h4>STUDIO PLUNE</h4>
+                <h4>Septembre 2020 - Novembre 2021</h4>
+            </div>
+            <div class="exp-desc">
+                <p> - Intégration sur blog Wordpress (HTML/CSS)<br>
+                    - Utilisation du CMS PrestaBlog<br>
+                    - Développements d’éléments à intégrer sur WordPress (PHP)
+                </p>
+            </div>
+        </div>
+        <div class="exp">
+            <div class="exp-logo">
+                <a href="#"><img src="https://particuliers.engie.fr/content/dam/particuliers-engie/icones/partenaires/ima-protect-logo.jpg" alt="Logo de LegalPlace"></a>
+            </div>
+            <div class="exp-info">
+                <h3>Conseiller client</h3>
+                <h4>IMA PROTECT</h4>
+                <h4>Juin 2019 - Janvier 2020</h4>
+            </div>
+            <div class="exp-desc">
+                <p>Organiser et planifier tout type de rendez-vous conjointement avec le client et les techniciens<br>
+                    Conforter et conseiller le client dans son achat et valider son besoin et ses demandes complémentaires<br>
+                    Être l'interface entre les clients et les services internes de l'entreprise<br>
+                    Solutionner les situations imprévues et les cas particuliers</p>
+            </div>
+        </div>
+        <div class="exp">
+            <div class="exp-logo">
+                <a href="#"><img src="https://www.faxinfo.fr/wp-content/uploads/2020/01/24-01-2020-recrutement.jpg" alt="Logo de PrestaShop"></a>
+            </div>
+            <div class="exp-info">
+                <h3>Stagiaire en classe de CM2</h3>
+                <h4>ESPE de Nantes</h4>
+                <h4>Octobre 2018 - Mai 2019</h4>
+            </div>
+            <div class="exp-desc">
+                <p>Observation de séances effectuées par le professeur des écoles référent<br>
+                    Mener hebdomadairement différentes séances dans une classe de CM2 <br>
+                    Adopter un esprit critique sur la réalisation de mes séances.</p>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <h2>Formation</h2>
+        <div class="exp">
+            <div class="exp-logo">
+                <a href="#"><img src="https://arinfo.fr/images/logos/logo_Arinfo_RVB.png" alt="Logo EDHEC"></a>
+            </div>
+            <div class="exp-info">
+                <h3>Arinfo - Développeur web et web mobile</h3>
+                <h4>Novembre 2020 - Novembre 2021</h4>
+            </div>
+
+        </div>
+        <div class="exp">
+            <div class="exp-logo">
+                <a href="#"><img src="https://lh3.googleusercontent.com/proxy/Ogtx7bnZS9KFBsK-hXryzEBlnmr3D6th4zZ6J9gRgG9pvtfMlFpEvWhharjaXuiPkbzzXjs8bnPmPbRPTJyVXlG0J6V1tCK8jQ2TsfMPUFXUxzGDYzU1-iskgqYFbkxK" alt="Logo Dumont"></a>
+            </div>
+            <div class="exp-info">
+                <h3>Université de Nantes - Master MEEF</h3>
+                <h4>2017 - 2019</h4>
+            </div>
+
+        </div>
+        <div class="exp">
+            <div class="exp-logo">
+                <a href=""><img src="https://lh3.googleusercontent.com/proxy/Ogtx7bnZS9KFBsK-hXryzEBlnmr3D6th4zZ6J9gRgG9pvtfMlFpEvWhharjaXuiPkbzzXjs8bnPmPbRPTJyVXlG0J6V1tCK8jQ2TsfMPUFXUxzGDYzU1-iskgqYFbkxK" alt=""></a>
+            </div>
+            <div class="exp-info">
+                <h3>Université de Nantes - Licence de Sociologie</h3>
+                <h4>2014 - 2017</h4>
+            </div>
+
+        </div>
+    </section>
+
+    <section>
+        <h2>Compétences</h2>
+        <h3 class="h3gauche">Professionnelles</h3>
+        <div class="comp">
+            <p>HTML / CSS</p>
+            <div class="conteneur-barre"><span class="barre c100"></span></div>
+        </div>
+        <div class="comp">
+            <p>PHP / MySQL</p>
+            <div class="conteneur-barre"><span class="barre c95"></span></div>
+        </div>
+
+        <div class="comp">
+            <p>SEO</p>
+            <div class="conteneur-barre"><span class="barre c75"></span></div>
+        </div>
+        <h3 class="h3gauche">Personnelles</h3>
+        <div class="allDiv1">
+            <div class="div1-comp2">
+                <div class="comp2">
+                    <p>💪🏼</p>
+                    <p>Motivé</p>
+                    <!-- <div class="conteneur-barre2"><span class="barre c90"></span></div> -->
+                </div>
+                <div class="comp2">
+                    <p>🔎</p>
+                    <p>Curieux</p>
+                    <!-- <div class="conteneur-barre2"><span class="barre c85"></span></div> -->
+                </div>
+            </div>
+            <div class="div1-comp2">
+                <div class="comp2">
+                    <p>✨</p>
+                    <p>Sérieux</p>
+                    <!-- <div class="conteneur-barre2"><span class="barre c95"></span></div> -->
+                </div>
+                <div class="comp2">
+                    <p>🎓</p>
+                    <p>Pédagogue</p>
+                    <!-- <div class="conteneur-barre2"><span class="barre c95"></span></div> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <h2>Centres d'intérêt</h2>
+        <figure class="interet">
+            <img src="https://image.flaticon.com/icons/png/512/1074/1074123.png" alt="Trail">
+            <figcaption>Veille technologique</figcaption>
+        </figure>
+        <figure class="interet">
+            <img src="https://image.freepik.com/vecteurs-libre/console-lettrage-jeux-neon-fond-brique_1262-11854.jpg" alt="Cuisine">
+            <figcaption>Esport</figcaption>
+        </figure>
+        <figure class="interet">
+            <img src="https://image.flaticon.com/icons/png/512/1191/1191108.png" alt="Littérature">
+            <figcaption>RPG / FPS</figcaption>
+        </figure>
+    </section>
+
+    <footer>
+        <p><a href="https://www.pierre-giraud.com">©Pierre TREMBLAY</a> 2020</p>
+        <p>Reproduction à des fins commerciales interdite.</p>
+        <p>Merci de respecter le travail des auteurs en faisant un lien vers
+            le contenu original !</p>
+    </footer>
+</body>
